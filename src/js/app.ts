94e513cb827e7c29467c712c9280a5b397958fea
Даниил <danilka.volkov.02@mail.ru>
2025-04-19 15:12:38 +0300

    .

---
+++ src/js/app.ts
@@ -0,0 +1,27 @@
+import Swiper from 'swiper'
+import {Pagination} from 'swiper/modules'
+
+import 'swiper/swiper-bundle.min.css'
+
+new Swiper('.banner__swiper', {
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 40,
+  pagination: {
+    el: '.banner__paginator',
+    clickable: true,
+  },
+  modules: [Pagination],
+});
+
+new Swiper('.works__swiper', {
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 40,
+  breakpoints: {
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 34
+    }
+  }
+});
