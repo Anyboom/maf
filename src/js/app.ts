d9f20ab7669302ed65a551efc673408f1209b3c0
Даниил <danilka.volkov.02@mail.ru>
2025-05-02 15:04:23 +0300

    Обновил

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,15 +1,24 @@
-import Swiper from 'swiper'
-import {Pagination} from 'swiper/modules'
+import Swiper from 'swiper';
+import { Pagination, Autoplay } from 'swiper/modules';
+import Alpine from 'alpinejs';
+
+window["Alpine"] = Alpine;
+
+Alpine.start();
 
 new Swiper('.banner__swiper', {
   direction: 'horizontal',
   loop: true,
   spaceBetween: 40,
+  autoplay: {
+    delay: 5000,
+    pauseOnMouseEnter: true,
+  },
   pagination: {
     el: '.banner__paginator',
     clickable: true,
   },
-  modules: [Pagination],
+  modules: [Pagination, Autoplay],
 });
 
 new Swiper('.works__swiper', {
@@ -20,6 +29,17 @@ new Swiper('.works__swiper', {
     1280: {
       slidesPerView: 4,
       spaceBetween: 34
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 2,
     }
-  }
+  },
+  autoplay: {
+    delay: 2500,
+    pauseOnMouseEnter: true,
+  },
+  modules: [Autoplay],
 });
